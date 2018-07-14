03b5d7dfe97c205020e4a97c740beb0d512c3a05
Ahmad Al-kheat <ahmad@himama.com>
2018-07-14 10:49:57 -0400

    create app

---
+++ config.py
@@ -0,0 +1,17 @@
+import os
+
+class Config:
+	SECRET_KEY = os.environ.get('SECRET_KEY') or 'wf9023j9'
+
+	@staticmethod
+	def init_app(app):
+		pass
+
+class DevelopmentConfig(Config):
+	Debug = True
+
+config = {
+	'development': DevelopmentConfig,
+
+	'default': DevelopmentConfig
+}
