95d38197f45b0234a93ae8b1ddb183e54b05a762
Ahmad Al-kheat <ahmad@himama.com>
2018-07-15 13:19:27 -0400

    db config

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'wf9023j9'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     @staticmethod
     def init_app(app):
@@ -9,6 +10,7 @@ class Config:
 
 class DevelopmentConfig(Config):
     Debug = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL')
 
 config = {
     'development': DevelopmentConfig,
